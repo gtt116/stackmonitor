23d0ad8dd05aca078c4f513383ad6fd70509c38b
TianTian <gtt116@126.com>
2013-06-06 17:47:17 +0800

    Add script for nova-api.

---
+++ check_nova_api.py
@@ -0,0 +1,31 @@
+#-*- encoding: utf-8 -*-
+from novaclient.v1_1 import client as novaclient
+
+import utils
+import keystone
+
+
+DEBUG = utils.get_debug()
+
+try:
+    user = keystone.Keystone(*utils.get_token_config())
+    username, password, tenant_name, keystone_url = utils.get_token_config()
+    nova_url = '%s/v1.1/%s' % (utils.get_nova_url(), user.get_tenant_id())
+    nova = novaclient.Client(username, password, tenant_name,
+                                                    keystone_url, nova_url)
+
+    if DEBUG:
+        print username, password, tenant_name, keystone_url, nova_url
+
+    vms = nova.servers.list()
+
+    if DEBUG:
+        print vms
+except Exception:
+    if DEBUG:
+        utils.print_traceback()
+    result = 'failed'
+else:
+    result = 'success'
+
+utils.print_result(result)
