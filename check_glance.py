d3e9de5ad9fcd97cdc1ad14206daf47995a59830
TianTian <gtt116@126.com>
2013-06-06 15:02:14 +0800

    Make check_glance match format of om.

--- check_glance.py
+++ check_glance.py
@@ -1,3 +1,4 @@
+#-*- encoding: utf-8 -*-
 #!/usr/bin/env python
 # configs
 username = 'demo'
@@ -11,25 +12,30 @@ from glanceclient import exc as glanceexc
 from keystoneclient.v2_0 import client as keyclient
 
 
-keystone = keyclient.Client(username=username, password=password,
-                            tenant_name=tenant_name, auth_url=keystone_url)
-token = keystone.auth_token
-
-glance = glanceclient.Client(endpoint=glance_url, token=token)
-
-data = 'balbal' * 100
-image = glance.images.create(name='__nvs_monitor__', data=data,
-                             disk_format='qcow2', container_format='ovf',
-                             is_public=False)
-
-if image.status != 'active':
-    print 'create image error. %s' % image
-print 'create image success ..'
-
-image.delete()
+result = 'success'
 
 try:
-    image.get()
-except glanceexc.HTTPNotFound:
-    pass
-print 'delete image success ..'
+    keystone = keyclient.Client(username=username, password=password,
+                                tenant_name=tenant_name, auth_url=keystone_url)
+    token = keystone.auth_token
+
+    glance = glanceclient.Client(endpoint=glance_url, token=token)
+
+    data = 'balbal' * 100
+    image = glance.images.create(name='__nvs_monitor__', data=data,
+                                disk_format='qcow2', container_format='ovf',
+                                is_public=False)
+
+    if image.status != 'active':
+        print 'create image error. %s' % image
+
+    image.delete()
+
+    try:
+        image.get()
+    except glanceexc.HTTPNotFound:
+        pass
+except Exception:
+    result = 'failed'
+
+print 'result %s != success ${key}的值${value}${operator}${limit},脚本运行时间为${time}秒' % result
