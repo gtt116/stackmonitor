7678d8ab68924f725d9af4b868bd23ec6d0040e0
TianTian <gtt116@126.com>
2013-06-06 17:52:16 +0800

    Using no-cache argument.

--- check_nova_api.py
+++ check_nova_api.py
@@ -12,7 +12,7 @@ try:
     username, password, tenant_name, keystone_url = utils.get_token_config()
     nova_url = '%s/v1.1/%s' % (utils.get_nova_url(), user.get_tenant_id())
     nova = novaclient.Client(username, password, tenant_name,
-                                                    keystone_url, nova_url)
+                                    keystone_url, nova_url, no_cache=True)
 
     if DEBUG:
         print username, password, tenant_name, keystone_url, nova_url
