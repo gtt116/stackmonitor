f0fbb0499941c6ebad74a23b3e01818afd5b40e3
TianTian <gtt116@126.com>
2013-06-06 15:43:10 +0800

    Make glance script more graceful.

--- check_glance.py
+++ check_glance.py
@@ -1,16 +1,54 @@
 #-*- encoding: utf-8 -*-
-#!/usr/bin/env python
-# configs
-username = 'demo'
-password = 'demo'
-tenant_name = 'demo'
-keystone_url = 'http://localhost:5000/v2.0'
-glance_url = 'http://localhost:9292'
+
+DEBUG = False
+
+import ConfigParser
 
 from glanceclient.v1 import client as glanceclient
 from glanceclient import exc as glanceexc
 from keystoneclient.v2_0 import client as keyclient
 
+
+def read_keystone_rc():
+    cc = ConfigParser.ConfigParser()
+    cc.readfp(file('/etc/nova/api-paste.ini'))
+
+    username = cc.get('filter:authtoken', 'admin_user')
+    password = cc.get('filter:authtoken', 'admin_password')
+    tenant_name = cc.get('filter:authtoken', 'admin_tenant_name')
+    try:
+        auth_uri = cc.get('filter:authtoken', 'auth_uri')
+    except Exception:
+        auth_uri = 'http://0.0.0.0:5000'
+
+    if -1 == auth_uri.rfind('/v2.0'):
+        auth_uri = '%s/v2.0' % auth_uri
+
+    cc.readfp(file('/etc/glance/glance-api.conf'))
+    try:
+        host = cc.get('DEFAULT', 'bind_host')
+    except Exception:
+        host = '0.0.0.0'
+
+    try:
+        port = cc.get('DEFAULT', 'bind_port')
+    except Exception:
+        port = '9292'
+
+    glance_url = 'http://%s:%s/' % (host, port)
+
+    return (username, password, tenant_name, auth_uri, glance_url)
+
+username, password, tenant_name, keystone_url, glance_url = read_keystone_rc()
+
+if DEBUG:
+    print username, password, tenant_name, keystone_url, glance_url
+
+#username = 'demo'
+#password = 'demo'
+#tenant_name = 'demo'
+#keystone_url = 'http://localhost:5000/v2.0'
+#glance_url = 'http://localhost:9292'
 
 result = 'success'
 
@@ -18,11 +56,9 @@ try:
     keystone = keyclient.Client(username=username, password=password,
                                 tenant_name=tenant_name, auth_url=keystone_url)
     token = keystone.auth_token
-
     glance = glanceclient.Client(endpoint=glance_url, token=token)
 
-    data = 'balbal' * 100
-    image = glance.images.create(name='__nvs_monitor__', data=data,
+    image = glance.images.create(name='__nvs_monitor__', data='a' * 1024,
                                 disk_format='qcow2', container_format='ovf',
                                 is_public=False)
 
@@ -35,7 +71,10 @@ try:
         image.get()
     except glanceexc.HTTPNotFound:
         pass
-except Exception:
+except Exception, ex:
+    if DEBUG:
+        import traceback
+        traceback.print_exc()
     result = 'failed'
 
 print 'result %s != success ${key}的值${value}${operator}${limit},脚本运行时间为${time}秒' % result
