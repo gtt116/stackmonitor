8964a65337c751ed1f3966bf33a97fcbf191bdd2
TianTian <gtt116@126.com>
2013-06-07 12:01:07 +0800

    Move all rpc code into utils

--- check_nova_network.py
+++ check_nova_network.py
@@ -6,7 +6,6 @@ Get all network info from nova-network through RPC call to make
 sure it is health.
 """
 
-from nova.openstack.common import rpc
 import nova_utils
 import utils
 
@@ -14,16 +13,7 @@ DEBUG = utils.get_debug()
 
 try:
     nova_utils.init_nova()
-    ctxt = nova_utils.get_admin_context()
-
-    net_topic = nova_utils.get_network_topic()
-    host = nova_utils.get_hostname()
-    utils.log(net_topic)
-    utils.log(host)
-
-    network_info = rpc.call(ctxt,
-                            rpc.queue_get_for(ctxt, net_topic, host),
-                            {'method': 'get_all_networks'})
+    network_info = nova_utils.rpccall_network('get_all_networks')
     assert network_info
     utils.log(network_info)
 except Exception:
--- nova_utils.py
+++ nova_utils.py
@@ -2,8 +2,10 @@ import random
 from nova import flags
 from nova import db
 from nova import context
+from nova.openstack.common import rpc
 
 CONF = flags.cfg.CONF
+rpc_timeout = 10
 
 
 def init_nova():
@@ -36,9 +38,31 @@ def get_random_host():
     return hosts[0]
 
 
-def get_network_topic():
+def get_network_topic(host=None):
+    c = get_admin_context()
+    if host:
+        return rpc.queue_get_for(c, CONF.network_topic, host)
     return CONF.network_topic
 
 
-def get_compute_topic():
+def get_compute_topic(host=None):
+    c = get_admin_context()
+    if host:
+        return rpc.queue_get_for(c, CONF.compute_topic, host)
     return CONF.compute_topic
+
+
+def rpccall_network(method, **kwargs):
+    c = get_admin_context()
+    host = get_hostname()
+    return rpc.call(c, get_network_topic(host),
+                    {'method': method, 'args': kwargs},
+                    timeout=rpc_timeout)
+
+
+def rpccall_compute(method, **kwargs):
+    c = get_admin_context()
+    host = get_hostname()
+    return rpc.call(c, get_compute_topic(host),
+                    {'method': method, 'version': '2.0', 'args': kwargs},
+                    timeout=rpc_timeout)
